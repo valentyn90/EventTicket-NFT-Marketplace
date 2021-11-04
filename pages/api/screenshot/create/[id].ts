f79b1556c65f5c10163cde9eb653a967447bfe23
TopDev-727 <topdev997@gmail.com>
2021-11-04 19:50:17 -0400

    Vercel screenshot (#89)
    * Trying to fix lambda screenshot issue
    
    * smaller file hopefully
    
    * fix to old version
    
    * removing unused dependencies

--- pages/api/screenshot/create/[id].ts
+++ pages/api/screenshot/create/[id].ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-const { chromium } = require('playwright');
+import chromium from 'chrome-aws-lambda';
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,14 +8,24 @@ export default async function handler(
   const id = req.query.id as string;
 
   try {
-    const browser = await chromium.launch();
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    })
     const page = await browser.newPage();
-    await page.goto(`https://verifiedink.us/screenshot/${id}`, { waitUntil: 'networkidle' });
+    await page.goto(`https://verifiedink.us/screenshot/${id}`, { waitUntil: 'networkidle0' });
     const elementHandle = await page.$('.card-container');
-    const data = await elementHandle.screenshot({ omitBackground: true });
-    await browser.close();
-    var base64data = "data:image/png;base64, " + Buffer.from(data).toString("base64");
-    res.send(base64data);
+    const data = await elementHandle?.screenshot({ omitBackground: true });
+
+    await browser.close(); 
+    if(data) {
+      var base64data = "data:image/png;base64, " + Buffer.from(data).toString("base64");
+      res.send(base64data);
+    }
+    else {
+      res.status(400);
+    }
   }
   catch (err) {
     console.log(err);
