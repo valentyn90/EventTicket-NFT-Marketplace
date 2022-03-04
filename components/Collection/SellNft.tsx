4f1877728073324373bad83777a379fb1d2a05f2
TopDev-727 <topdev997@gmail.com>
2022-03-04 09:16:16 +0700

    Listing nft shows modal dialogue (#171)
    * listing nft shows modal dialogue
    
    * Tweaks to wording.
    
    Co-authored-by: Aaron Linsky <aaron.linsky@gmail.com>

--- components/Collection/SellNft.tsx
+++ components/Collection/SellNft.tsx
@@ -9,7 +9,8 @@ import {
   useToast,
   VStack,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+import MintingModal from "../ui/MintingModal";
 
 interface Props {
   solSellPrice: string;
@@ -24,7 +25,7 @@ interface Props {
     nft_id: number,
     selectedSN: number,
     setOrderBook: React.Dispatch<React.SetStateAction<OrderBook | null>>
-  ) => void;
+  ) => Promise<void>;
   listingNft: boolean;
 }
 
@@ -40,90 +41,105 @@ const SellNft: React.FC<Props> = ({
   listingNft,
 }) => {
   const toast = useToast();
+
+  const [openBuyModal, setOpenBuyModal] = useState(false);
+
   return (
-    <VStack align={`start`} spacing={8} w="100%">
-      <HStack justify="start" w="100%">
-        <Text flex="2" fontSize={["l", "l", "2xl"]}>
-          List Price:
-        </Text>
+    <>
+      <VStack align={`start`} spacing={8} w="100%">
+        <HStack justify="start" w="100%">
+          <Text flex="2" fontSize={["l", "l", "2xl"]}>
+            List Price:
+          </Text>
 
-        <Flex flex="2" borderBottom="0.5px solid gray" align="center">
-          <input
-            type="number"
-            className="sol-sell-price-input"
-            value={solSellPrice}
-            placeholder="0.1"
-            onChange={(e) => setSolSellPrice(e.target.value)}
-          />
-        </Flex>
-        <Text flex="1" fontSize={"2xl"} textAlign={"start"}>
-          SOL
-        </Text>
+          <Flex flex="2" borderBottom="0.5px solid gray" align="center">
+            <input
+              type="number"
+              className="sol-sell-price-input"
+              value={solSellPrice}
+              placeholder="0.1"
+              onChange={(e) => setSolSellPrice(e.target.value)}
+            />
+          </Flex>
+          <Text flex="1" fontSize={"2xl"} textAlign={"start"}>
+            SOL
+          </Text>
 
-        <Text flex="2" color={"gray"} fontSize={"2xl"}>
-          {solPrice !== 0 &&
-            solSellPrice != "" &&
-            `($${(solPrice * Number(solSellPrice)).toFixed(2)})`}
-        </Text>
-      </HStack>
-      <Button
-        w="100%"
-        colorScheme={"blue"}
-        color="white"
-        onClick={() => {
-          const sellSolPrice = Number(solSellPrice);
+          <Text flex="2" color={"gray"} fontSize={"2xl"}>
+            {solPrice !== 0 &&
+              solSellPrice != "" &&
+              `($${(solPrice * Number(solSellPrice)).toFixed(2)})`}
+          </Text>
+        </HStack>
+        <Button
+          w="100%"
+          colorScheme={"blue"}
+          color="white"
+          onClick={() => {
+            const sellSolPrice = Number(solSellPrice);
 
-          if (sellSolPrice <= 0) {
-            toast({
-              position: "top",
-              description: "Enter a sell price.",
-              status: "error",
-              duration: 3000,
-              isClosable: true,
-            });
-            return;
+            if (sellSolPrice <= 0) {
+              toast({
+                position: "top",
+                description: "Enter a sell price.",
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+              });
+              return;
+            }
+
+            const nftId = nftOwnerDetails.find(
+              (detail) =>
+                detail.serial_no === selectedSN && detail.nft_id === nft_id
+            )?.nft_id;
+
+            if (!nftId) {
+              toast({
+                position: "top",
+                description: "There was an error finding your NFT.",
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+              });
+              return;
+            }
+
+            setOpenBuyModal(true);
+
+            handleListNftForSale(
+              sellSolPrice,
+              nftId,
+              selectedSN,
+              setSelectedOrder
+            )
+              .then(() => {
+                setOpenBuyModal(false);
+              })
+              .catch((err) => {
+                console.log(err);
+                setOpenBuyModal(false);
+              });
+          }}
+          disabled={
+            listingNft ||
+            solSellPrice === "" ||
+            process.env.NEXT_PUBLIC_ENABLE_MARKETPLACE === "false"
           }
-
-          const nftId = nftOwnerDetails.find(
-            (detail) =>
-              detail.serial_no === selectedSN && detail.nft_id === nft_id
-          )?.nft_id;
-
-          if (!nftId) {
-            toast({
-              position: "top",
-              description: "There was an error finding your NFT.",
-              status: "error",
-              duration: 3000,
-              isClosable: true,
-            });
-            return;
-          }
-
-          handleListNftForSale(
-            sellSolPrice,
-            nftId,
-            selectedSN,
-            setSelectedOrder
-          );
-        }}
-        disabled={
-          listingNft ||
-          solSellPrice === "" ||
-          process.env.NEXT_PUBLIC_ENABLE_MARKETPLACE === "false"
-        }
-      >
-        {listingNft ? (
-          <Spinner />
-        ) : (
-          `${
-            solSellPrice === ""
-              ? "Waiting for List Price..."
-              : "List for " + solSellPrice + " SOL"
-          } `
-        )}
-      </Button>
-    </VStack>
+        >
+          {listingNft ? (
+            <Spinner />
+          ) : (
+            `${
+              solSellPrice === ""
+                ? "Waiting for List Price..."
+                : "List for " + solSellPrice + " SOL"
+            } `
+          )}
+        </Button>
+      </VStack>
+      <MintingModal isOpen={openBuyModal} setIsOpen={setOpenBuyModal} />
+    </>
   );
 };
 
---
+++ components/ui/MintingModal.tsx
@@ -0,0 +1,96 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Text,
+} from "@chakra-ui/react";
+import styled, { keyframes } from "styled-components";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+const ellipsis = keyframes`
+  0%   { content: ''; }
+  25%  { content: '.'; }
+  50%  { content: '..'; }
+  75%  { content: '...'; }
+  100% { content: ''; }
+`;
+
+const Loader = styled.p`
+  &:after {
+    display: inline-block;
+    animation: ${ellipsis} steps(1, end) 2s infinite;
+    content: "";
+  }
+`;
+
+interface Props {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const MintingModal: React.FC<Props> = ({ isOpen, setIsOpen }) => {
+  const onClose = () => setIsOpen(false);
+  const cancelRef = useRef(null);
+
+  const [countdown, setCountdown] = useState(30);
+
+  useEffect(() => {
+    let interval: any = null;
+    if (isOpen) {
+      interval = setInterval(() => {
+        if (countdown > 0) {
+          setCountdown((countdown) => countdown - 1);
+        } else {
+          clearInterval(interval);
+        }
+      }, 1000);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isOpen, countdown]);
+
+  return (
+    <AlertDialog
+      isCentered={true}
+      isOpen={isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+      closeOnOverlayClick={false}
+      closeOnEsc={false}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader
+            fontSize="2xl"
+            fontWeight="bold"
+          >
+            Listing Your VerifiedInk
+            </AlertDialogHeader>
+
+          <AlertDialogBody>
+            Your VerifiedInk is minting then listing on the Solana Network.
+            <br/><br/>
+            Please do not navigate to another page, this can sometimes take up to 30 seconds.
+          </AlertDialogBody>
+          <AlertDialogBody>
+            {countdown > 0 ? `Time to list: ${countdown}s` : <Loader>Just a little longer</Loader>}
+          </AlertDialogBody>
+
+          <AlertDialogFooter>
+            {/* <Button ref={cancelRef} onClick={onClose}>
+              Ok
+            </Button> */}
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  );
+};
+
+export default MintingModal;
