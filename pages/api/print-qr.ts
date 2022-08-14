8c201c79ab0c185e347df2b49743500e62445529
TopDev-727 <topdev997@gmail.com>
2022-08-14 16:03:01 -0400

    New AR Card Generation

--- pages/api/print-qr.ts
+++ pages/api/print-qr.ts
@@ -8,7 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // const page = await browser.newPage({
     //     viewport: {width: 1000,height: 1000}
     // })
-    // for (let i = 651; i < 1001; i++) {
+    // for (let i = 1347; i < 1501; i++) {
     //     await page.goto(`http://localhost:3000/screenshot/qr?id=${i}`)
     //     await page.locator('id=card').screenshot({path: `qr/${i}.png`, omitBackground: true})
     // }
--- pages/screenshot/qr.tsx
+++ pages/screenshot/qr.tsx
@@ -9,20 +9,20 @@ const Wrapper = styled.div`
   margin: 0px !important;
   // padding: 40px !important;
   position: relative;
-  width: 500px;
-  height: 824.47px;
+  width: 1900px;
+  height: 3133px;
 
   #react-qrcode-logo {
     position: relative;
-    top: 357px;
-    left: 167px;
+    top: 1383px;
+    left: 653px;
     opacity: 0.75;
     backdrop-filter: blur(12px) brightness(0.85);
   }
 
   .target {
     position: absolute;
-    width: 500px;
+    width: 1900px;
   }
 
   .text{
@@ -33,10 +33,11 @@ const Wrapper = styled.div`
   }
   .id{
     position: absolute;
-    top: 525px;
-    left: 219px;
+    top: 2000px;
+    left: 868px;
     opacity: 0.7;
     transform-origin: top left;
+    font-size: 50px;
     // transform: rotate(-40deg);
     // backdrop-filter: blur(12px);
     
@@ -63,7 +64,7 @@ const Screenshot: React.FC = () => {
     <Wrapper id="card">
       <img className="target" src={back ? '/ar/source/target-back.png' : '/ar/source/target-front.png'}></img>
       {back ? null :
-        <QRCode size={150} bgColor="transparent" fgColor="white" value={`https://verifiedink.us/ar?ar_id=${id}`} qrStyle="dots" />
+        <QRCode size={600} bgColor="transparent" fgColor="white" value={`https://verifiedink.us/ar?ar_id=${id}`} qrStyle="dots" />
       }
       <text className="text">Scan Me</text>
       {back ? null : <text className="id">No. {id}</text>}
