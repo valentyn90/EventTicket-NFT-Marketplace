a18584eadf85b5a072d29430d5422b7b7b51b85e
TopDev-727 <topdev997@gmail.com>
2022-09-19 19:02:35 -0400

    More sql

--- scripts/sql.sql
+++ scripts/sql.sql
@@ -11,3 +11,52 @@ update nft
 set (mux_playback_id, mux_upload_id, mux_asset_id, mux_max_resolution, crop_values)
 = (select mux_playback_id, mux_upload_id, mux_asset_id, mux_max_resolution, crop_values from nft where id = 1599) 
 where id = 1543 --Replace this video with the one above
+
+
+
+select * from public.drop where id=6
+
+
+-- 348d305f-3156-44ec-98f6-5d052bea2aa8 - VerifiedId
+
+-- Premium - 1   / 10
+-- Common  - 44  / 445
+-- Rare    - 4   / 40
+-- Leg     - 1   / 15
+
+{1607,1608,1609}	1543	'7472d65f-d77d-474e-9e59-f41418474d4a'	
+
+Delete from
+--select * from 
+nft_owner where nft_id in (1543,1607,1608,1609)
+
+select id, edition_quantity, edition_name, * from nft where id in (1543,1607,1608,1609)
+
+--Launch
+insert into nft_owner(nft_id, owner_id, serial_no)
+select 1543 as nft_id, UUID('7472d65f-d77d-474e-9e59-f41418474d4a') as owner_id, generate_series as serial_no from generate_series(1,9)
+UNION ALL
+select  1543 as nft_id, UUID('348d305f-3156-44ec-98f6-5d052bea2aa8') as owner_id, generate_series as serial_no from generate_series(10,10)
+
+-- Common
+insert into nft_owner(nft_id, owner_id, serial_no)
+select 1607 as nft_id, UUID('7472d65f-d77d-474e-9e59-f41418474d4a') as owner_id, generate_series as serial_no from generate_series(1,401)
+UNION ALL
+select  1607 as nft_id, UUID('348d305f-3156-44ec-98f6-5d052bea2aa8') as owner_id, generate_series as serial_no from generate_series(402,445)
+
+--Rare
+insert into nft_owner(nft_id, owner_id, serial_no)
+select 1608 as nft_id, UUID('7472d65f-d77d-474e-9e59-f41418474d4a') as owner_id, generate_series as serial_no from generate_series(1,36)
+UNION ALL
+select  1608 as nft_id, UUID('348d305f-3156-44ec-98f6-5d052bea2aa8') as owner_id, generate_series as serial_no from generate_series(37,40)
+
+--Lego
+insert into nft_owner(nft_id, owner_id, serial_no)
+select 1609 as nft_id, UUID('7472d65f-d77d-474e-9e59-f41418474d4a') as owner_id, generate_series as serial_no from generate_series(1,14)
+UNION ALL
+select  1609 as nft_id, UUID('348d305f-3156-44ec-98f6-5d052bea2aa8') as owner_id, generate_series as serial_no from generate_series(15,15)
+
+update nft 
+set minted = true, mint_datetime=now()
+--select minted, mint_datetime from nft
+where id in (1543,1607,1608,1609)
