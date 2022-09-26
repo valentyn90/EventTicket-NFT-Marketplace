d85b65cc06ca34be7e12104e74bc103d9a67ba8e
TopDev-727 <topdev997@gmail.com>
2022-09-25 20:59:40 -0400

    updating script

--- scripts/sql.sql
+++ scripts/sql.sql
@@ -2,9 +2,27 @@
 INSERT INTO public.nft(
 	inserted_at, updated_at, user_id, first_name, last_name, graduation_year, high_school, usa_state, sport, sport_position, video_link, quotes, finished, approved, minted, signature_file, photo_file, clip_file, mux_playback_id, mux_upload_id, mux_asset_id, screenshot_file_id, user_details_id, mint_datetime, color_top, color_bottom, color_transition, mux_max_resolution, og_photo_id, crop_keyframes, slow_video, crop_values, admin_feedback, post_hs, vfd_year, edition_name, edition_quantity, edition_rarity, edition_utility, temp_user_id)
 	
-	Select inserted_at, updated_at, user_id, first_name, last_name, graduation_year, high_school, usa_state, sport, sport_position, video_link, quotes, finished, approved, minted, signature_file, photo_file, clip_file, mux_playback_id, mux_upload_id, mux_asset_id, screenshot_file_id, user_details_id, mint_datetime, color_top, color_bottom, color_transition, mux_max_resolution, og_photo_id, crop_keyframes, slow_video, crop_values, admin_feedback, post_hs, vfd_year, edition_name, edition_quantity, edition_rarity, edition_utility, temp_user_id
+	Select inserted_at, updated_at, user_id, first_name, last_name, graduation_year, high_school, usa_state, sport, sport_position, video_link, quotes, finished, approved, minted, signature_file, photo_file, clip_file, mux_playback_id, mux_upload_id, mux_asset_id, screenshot_file_id, user_details_id, 
+	mint_datetime, color_top, color_bottom, color_transition, mux_max_resolution, og_photo_id, crop_keyframes, slow_video, crop_values, admin_feedback, post_hs, vfd_year, 
+	'Extended', 445, 'Common', edition_utility, temp_user_id
 	from public.nft
-		where id=1600
+		where id=1586
+	UNION ALL
+	Select inserted_at, updated_at, user_id, first_name, last_name, graduation_year, high_school, usa_state, sport, sport_position, video_link, quotes, finished, approved, minted, signature_file, photo_file, clip_file, mux_playback_id, mux_upload_id, mux_asset_id, screenshot_file_id, user_details_id, 
+	mint_datetime, color_top, color_bottom, color_transition, mux_max_resolution, og_photo_id, crop_keyframes, slow_video, crop_values, admin_feedback, post_hs, vfd_year, 
+	'Extended', 40, 'Rare', edition_utility, temp_user_id
+	from public.nft
+		where id=1586
+	UNION ALL
+	Select inserted_at, updated_at, user_id, first_name, last_name, graduation_year, high_school, usa_state, sport, sport_position, video_link, quotes, finished, approved, minted, signature_file, photo_file, clip_file, mux_playback_id, mux_upload_id, mux_asset_id, screenshot_file_id, user_details_id, 
+	mint_datetime, color_top, color_bottom, color_transition, mux_max_resolution, og_photo_id, crop_keyframes, slow_video, crop_values, admin_feedback, post_hs, vfd_year, 
+	'Extended', 15, 'Legendary', edition_utility, temp_user_id
+	from public.nft
+		where id=1586
+
+update public.nft
+set edition_rarity = 'Legendary'
+where id = 1586
 
 --Replacing a video
 update nft 
