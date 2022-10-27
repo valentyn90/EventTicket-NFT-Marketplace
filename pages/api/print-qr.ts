17ba06fdad92513caa3521d66b001726a1860432
TopDev-727 <topdev997@gmail.com>
2022-10-27 16:42:25 -0400

    physical card increment

--- pages/api/print-qr.ts
+++ pages/api/print-qr.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 // import {chromium } from "playwright";
 // yarn add playwright -D if you need to install playwright
+// resized outputs to 2.385" at 600dpi
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
 
@@ -8,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // const page = await browser.newPage({
     //     viewport: {width: 1000,height: 1000}
     // })
-    // for (let i = 1347; i < 1501; i++) {
+    // for (let i = 1501; i < 2001; i++) {
     //     await page.goto(`http://localhost:3000/screenshot/qr?id=${i}`)
     //     await page.locator('id=card').screenshot({path: `qr/${i}.png`, omitBackground: true})
     // }
